aa3895c33de7b112083a56ef217628933e31bff6
Harshitha <notamharshitha@gmail.com>
2026-05-12 16:17:06 +0530

    frontend

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -8,7 +8,7 @@ function ContactPage(){
             msg:""
         },
         onSubmit:(values)=>{        
-            console.log(values)
+           // console.log(values)
         }
     })
    function displayToastMsg() {
